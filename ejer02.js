1c925cc779466d7b61763de4d8f359aa1d4e864b
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-05-16 21:54:57 -0500

    Avanzamos con el metodo para invertir texto

--- ejer02.js
+++ ejer02.js
@@ -1,6 +1,11 @@
 function Invertir()
 {
-    let palabra = document.getElementById("#palabra").value;
-    console.log(palabra);
-
+    let palabra = document.getElementById("palabra").value;
+    let pal_invert = "";
+    //console.log(palabra);
+    for(var i = palabra.length - 1; i >=0; i--)
+    {
+        pal_invert = pal_invert + palabra.charAt(i);
+    }
+    alert(pal_invert);
 }
