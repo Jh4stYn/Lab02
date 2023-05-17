0310838e5470c60acddb62cbf67690eb3f3e6897
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-05-16 22:19:58 -0500

    Decido usar split para crear un arreglo con el texto ingresado

--- ejer02.js
+++ ejer02.js
@@ -1,11 +1,9 @@
 function Invertir()
 {
     let palabra = document.getElementById("palabra").value;
+    const chars= palabra.split('');
     let pal_invert = "";
-    //console.log(palabra);
-    for(var i = palabra.length - 1; i >=0; i--)
-    {
-        pal_invert = pal_invert + palabra.charAt(i);
-    }
-    alert(pal_invert);
+    console.log(palabra);
+    alert(chars);
+    document.getElementById("pal_invert").innerHTML = pal_invert;
 }
