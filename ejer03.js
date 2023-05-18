7c9a350b617ec2d514415a9c1a21c8f68bf16f8f
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-05-18 12:10:30 -0500

    Creamos y declaramos nuestra variables

---
+++ ejer03.js
@@ -0,0 +1,6 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const seconds = document.getElementById("seconds");
+
+const aqp = '15 Aug 2022';
