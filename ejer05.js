3ce9597f7ffb70dfb23decf423cebabb343d2859
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-05-18 10:57:17 -0500

    Terminamos de crear el método para crear la tablas

--- ejer05.js
+++ ejer05.js
@@ -2,3 +2,23 @@ let numElementos;
 let miTabla = "<div><table><tr>";
 let sumatoria = 0;
 const numerosTabla = [];
+function generarTabla()
+{
+    numElementos = document.getElementById("numElemntos").value;
+    console.log(numElementos);
+    for (let i = 0; i < numElementos; i++) 
+    {
+        numerosTabla[i] = Math.floor(Math.random() * 200);
+    }
+    console.log(numerosTabla); 
+    let j = 0;
+    for (let i = 0; i < numerosTabla.length; i++) 
+    {
+        miTabla = miTabla + "<th>"+numerosTabla[i]+"</th>";
+        j++;
+        if(j % 5 == 0) 
+        {
+          miTabla = miTabla + "</tr><tr>"
+        }
+    }
+}
