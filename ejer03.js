11682c794b6255b04ec45c856409b1cb5b27490e
Jhastyn Jefferson Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2023-05-18 13:01:14 -0500

    Creamos un metodo para contar el tiempo

--- ejer03.js
+++ ejer03.js
@@ -4,3 +4,8 @@ const mins = document.getElementById("mins");
 const seconds = document.getElementById("seconds");
 
 const aqp = '15 Aug 2022';
+
+function countTimer(){
+    const aqpDate = new Date(aqp);
+    const ahora = new Date();
+}
